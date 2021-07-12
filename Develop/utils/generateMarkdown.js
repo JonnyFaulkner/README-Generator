ab0aa4e478ac140fcd204b7fce7ca2b3c492e6c5
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-07-11 22:16:08 -0500

    added table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(data) {
+
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -10,12 +11,19 @@ function renderLicenseLink(data) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(data) {
+  renderLicenseBadge(data)
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   renderLicenseSection(data)
   return `# ${data.title}
+
+  ## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
 
   ## Description
   ${data.description}
